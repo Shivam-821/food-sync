c5aa07d8b8fb8ded9761b1b43339edfa2b31d9ee
Utkarsh-Singh-u <utkarshsinghgkp2004@gmail.com>
2025-03-08 21:06:23 +0530

    home started

--- Frontend/src/Components/Navbar/Navbar.jsx
+++ Frontend/src/Components/Navbar/Navbar.jsx
@@ -1,23 +1,33 @@
 import './Navbar.css';
-import profileMale from '../../assets/male.avif';
-import profileFemale from '../../assets/female.avif';
+import profileMale from '../../assets/male.jpg';
+import profileFemale from '../../assets/female.jpg';
 import { useState } from 'react';
 
 const Navbar = () => {
-  const [isLogin, setIsLogin] = useState(true);
+  const [isLogin, setIsLogin] = useState(false);
   const [profile, setProfile] = useState('male');
+
+  const handleAuthToggle = () => {
+    setIsLogin(!isLogin); // Toggle login state
+  };
 
   return (
     <div className='nav'>
-      <div className='nav-logo'>FoodSync</div>
+      <div className='nav-logo'><a href='#'>FoodSync</a></div>
       <ul className='nav-menu'>
-        <li>Donation</li>
-        <li>Upcycle</li>
-        <li>Surplus Producer</li>
-        <li>Recipe</li>
-        <li>About</li>
-        <li>
-          <img className='rounded-full ' src={profile === 'male' ? profileMale : profileFemale} alt="Profile" />
+        <li className='hover:text-blue-900 cursor-pointer'><a href='#'>Donation</a><div class="absolute bottom-0 w-full h-1 bg-blue-900 hidden group-hover:block  transition-all duration-200"></div></li>
+        <li className='hover:text-blue-900 cursor-pointer'><a href='#'>Upcycle</a></li>
+        <li className='hover:text-blue-900 cursor-pointer'><a href='#'>Surplus Producer</a></li>
+        <li className='hover:text-blue-900 cursor-pointer'><a href='#'>Recipe</a></li>
+        <li className='hover:text-blue-900 cursor-pointer'><a href='#'>About</a></li>
+        <li className='hover:text-blue-900 cursor-pointer'>
+          {isLogin ? (
+            <img className='rounded-full size-12 min-w-12' src={profile === 'male' ? profileMale : profileFemale} alt="Profile" />
+          ) : (
+            <button onClick={handleAuthToggle} className="flex items-center border-3 border-gray-600 rounded-xl text-white py-2 px-4 rounded hover:bg-blue-900 transition duration-200">
+              Log in
+            </button>
+          )}
         </li>
       </ul>
     </div>
