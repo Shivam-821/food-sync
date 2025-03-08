d62b77d4d317c43b35b8971975971df231b8f328
Vivek-kumar-v <patelvivek6370@gmail.com>
2025-03-08 18:36:14 +0530

    ho gya

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 function App() {
-  return <div className="text-3xl font-bold underline">App</div>;
+  return <div className="bg-red-900">App hai</div>;
 }
 
 export default App;
