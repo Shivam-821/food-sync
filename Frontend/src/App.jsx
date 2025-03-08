ea97cd407ce83b12273c436655647561574a9439
Aniket-nitian <anik807621@gmail.com>
2025-03-09 02:22:01 +0530

    Sign Up page completed

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -2,11 +2,23 @@ import React from "react";
 import { Routes, Route } from "react-router-dom";
 import ProducerHome from "./pages/ProducerHome";
 import Home from "./pages/Home";
+import Donation from "./Components/Donation/Donation";
+import About from "./Components/About/About";
+import Recipe from "./Components/Recipe/Recipe";
+import SurplusProducer from "./Components/SurplusProducer/SurplusProducer";
+import Upcycle from "./Components/Upcycle/Upcycle";
+import SignUp from "./Components/Signup/SignUp";
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/producerHome" element={<ProducerHome />} />
+      <Route path="/donation" element={<Donation />} />
+      <Route path="/about" element={<About />} />
+      <Route path="/recipe" element={<Recipe />} />
+      <Route path="/surplusProducer" element={<SurplusProducer />} />
+      <Route path="/upcycle" element={<Upcycle />} />
+      <Route path="/signup" element={<SignUp />} />
     </Routes>
   );
 }
---
+++ Frontend/src/Components/SurplusProducer/SurplusProducer.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function SurplusProducer() {
+  return <div>SurplusProducer</div>;
+}
+
+export default SurplusProducer;
