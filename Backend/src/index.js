037077f57f3b1dc35fe597193269101c4a0ec0f5
Vivek-kumar-v <patelvivek6370@gmail.com>
2025-03-09 22:29:28 +0530

    done

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -12,7 +12,7 @@ const PORT = process.env.PORT || 3005;
 connectDB()
   .then(() => {
     app.listen(PORT, () => {
-      console.log(chalk.green(`Server is listening at port ${PORT}`));
+      console.log(chalk.green("Server is listening at port ${PORT})"));
     });
   })
   .catch((err) => {
