32405ac155f1591ad78249793cbc68daa3a0d16c
Aniket-nitian <anik807621@gmail.com>
2025-03-10 23:09:03 +0530

    All done

--- Frontend/src/Components/Recipe/Recipe.jsx
+++ Frontend/src/Components/Recipe/Recipe.jsx
@@ -153,7 +153,6 @@ const recipes = [
 ];
 
 const RecipeSection = () => {
-  const { t, i18n } = useTranslation();
   const [selectedCategory, setSelectedCategory] = useState("All Types");
 
   const filteredRecipes =
