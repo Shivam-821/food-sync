85fd2131f7aa075aca199f7b1f54d2b45fdb874c
Shivam-821 <shi821vam@gmail.com>
2025-03-17 18:37:46 +0530

    new ngo section added

---
+++ Backend/src/models/ngo.models.js
@@ -0,0 +1,139 @@
+import mongoose, { Schema } from "mongoose";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+
+const ngoSchema = new Schema(
+  {
+    ownerName: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    ngoName: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      validate: {
+        validator: function (value) {
+          return /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/.test(value);
+        },
+        message: (props) => `${props.value} is not a valid email!`,
+      },
+    },
+    phone: {
+      type: Number,
+      // required: true,
+      // unique: true,
+      validate: {
+        validator: function (value) {
+          return /^[6-9]\d{9}$/.test(value.toString());
+        },
+        message: (props) => `${props.value} is not a valid mobile number!`,
+      },
+    },
+    role: {
+      type: String,
+      required: true,
+    },
+    avatar: { type: String },
+    password: {
+      type: String,
+      // required: [true, "password is required"],
+    },
+    refreshToken: { type: String },
+    address: {
+      type: String,
+      required: true,
+    },
+    donationsMade: [
+      {
+        type: String,
+        ref: "Donation",
+      },
+    ],
+    requestMade: {
+      type: Boolean,
+      default: false,
+    },
+    donationRequested: [
+      {
+        item: {
+          type: Schema.Types.ObjectId,
+          ref: "Donation",
+        },
+        quantity: {
+          type: Number,
+        },
+        donorType: {
+          type: String,
+        },
+        donorId: {
+          type: Schema.type.ObjectId,
+          ref: donorType,
+        },
+      },
+    ],
+    donationsReceived: [
+        {
+            type: Schema.Types.ObjectId, 
+            ref: "Donation"
+        }
+    ],
+    feedbacks: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Feedback",
+      },
+    ],
+    requestMade: [
+      {
+        items: {
+          type: Schema.Types.ObjectId,
+          ref: "Donation",
+        },
+      },
+    ],
+    gamification: {
+      type: Schema.Types.ObjectId,
+      ref: "Gamification",
+    },
+  },
+  { timestamps: true }
+);
+
+consumerSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) return next();
+  this.password = await bcrypt.hash(this.password, 10);
+  next();
+});
+
+consumerSchema.methods.isPasswordCorrect = async function (password) {
+  return await bcrypt.compare(password, this.password);
+};
+
+consumerSchema.methods.generateAccessToken = function () {
+  return jwt.sign(
+    {
+      _id: this._id,
+      email: this.email,
+    },
+    process.env.ACCESS_TOKEN_SECRET,
+    { expiresIn: process.env.ACCESS_TOKEN_EXPIRY }
+  );
+};
+
+consumerSchema.methods.generateRefreshToken = function () {
+  return jwt.sign(
+    {
+      _id: this._id,
+    },
+    process.env.REFERESH_TOKEN_SECRET,
+    { expiresIn: process.env.REFERESH_TOKEN_EXPIRY }
+  );
+};
+
+export const Ngo = mongoose.model("Ngo", ngoSchema);
